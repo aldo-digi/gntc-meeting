fe194532ea33f8f85e91e9e104a1f053ecb28550
Daimhoti9 <73882220+Daimhoti9@users.noreply.github.com>
2024-03-18 02:07:21 +0100

    Update login.js

--- src/Pages/login.js
+++ src/Pages/login.js
@@ -83,7 +83,7 @@ const LoginForm = () => {
                 type="submit"
                 fullWidth
                 variant="contained"
-                 color="#00cc00"
+                color="primary"
                 sx={{ mt: 3, mb: 2 }}
                 onClick={handleSubmit}
             >
